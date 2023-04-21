ededaa8cf78ff208109a02469381a13acda2c787
luis28tri <116816788+luis28tri@users.noreply.github.com>
2023-04-21 01:51:08 -0400

    Add files via upload

---
+++ Programa 1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Programa_1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaludo = new System.Windows.Forms.Button();
+            this.LblMensaje = new System.Windows.Forms.Label();
+            this.btnDespedida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSaludo
+            // 
+            this.btnSaludo.Location = new System.Drawing.Point(262, 68);
+            this.btnSaludo.Name = "btnSaludo";
+            this.btnSaludo.Size = new System.Drawing.Size(75, 23);
+            this.btnSaludo.TabIndex = 0;
+            this.btnSaludo.Text = "Saludo";
+            this.btnSaludo.UseVisualStyleBackColor = true;
+            this.btnSaludo.Click += new System.EventHandler(this.btnSaludo_Click);
+            // 
+            // LblMensaje
+            // 
+            this.LblMensaje.AutoSize = true;
+            this.LblMensaje.Location = new System.Drawing.Point(33, 73);
+            this.LblMensaje.Name = "LblMensaje";
+            this.LblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.LblMensaje.TabIndex = 1;
+            this.LblMensaje.Text = "label1";
+            // 
+            // btnDespedida
+            // 
+            this.btnDespedida.Location = new System.Drawing.Point(262, 110);
+            this.btnDespedida.Name = "btnDespedida";
+            this.btnDespedida.Size = new System.Drawing.Size(75, 23);
+            this.btnDespedida.TabIndex = 2;
+            this.btnDespedida.Text = "Despedida";
+            this.btnDespedida.UseVisualStyleBackColor = true;
+            this.btnDespedida.Click += new System.EventHandler(this.btnDespedida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 189);
+            this.Controls.Add(this.btnDespedida);
+            this.Controls.Add(this.LblMensaje);
+            this.Controls.Add(this.btnSaludo);
+            this.Name = "Form1";
+            this.Text = "Programa 1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.Label LblMensaje;
+        private System.Windows.Forms.Button btnDespedida;
+    }
+}
+
---
+++ Programa 1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_1._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaludo_Click(object sender, EventArgs e)
+        {
+            LblMensaje.Text = "Hola a Todos!";
+        }
+
+        private void btnDespedida_Click(object sender, EventArgs e)
+        {
+            LblMensaje.Text = "Adios a todos";
+            this.Text = "Bye";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LblMensaje.Text = " ";
+        }
+    }
+}
